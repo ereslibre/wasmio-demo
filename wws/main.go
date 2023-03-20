fa302de40765233be935264aafd036efa8d78ac9
Rafael Fernández López <ereslibre@ereslibre.es>
2023-03-20 10:13:50 +0100

    Update

--- wws/main.go
+++ wws/main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	demo "github.com/saschagrunert/demo"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	demo.New().Run()
+	d := demo.New()
+
+	d.Setup(setup)
+	d.Cleanup(cleanup)
+
+	d.Add(installAndRunWWS(), "install-and-run-wws", "Installs and runs Wasm Workers Server")
+
+	d.Run()
+}
+
+func setup(ctx *cli.Context) error {
+	return nil
+}
+
+func cleanup(ctx *cli.Context) error {
+	return nil
+}
+
+func installAndRunWWS() *demo.Run {
+	r := demo.NewRun(
+		"Install and run Wasm Workers Server (wws)",
+	)
+
+	r.Step(demo.S(
+		"Install Wasm Workers Server (wws)",
+	), demo.S(
+		"curl -fsSL https://workers.wasmlabs.dev/install | bash -s -- --local",
+	))
+
+	return r
 }
