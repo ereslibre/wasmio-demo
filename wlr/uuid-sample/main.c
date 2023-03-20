1d47050092082626a39a903546f724d08230e8aa
Rafael Fernández López <ereslibre@ereslibre.es>
2023-03-20 17:33:30 +0100

    Update

--- wlr/uuid-sample/main.c
+++ wlr/uuid-sample/main.c
@@ -7,6 +7,11 @@ int main(int argc, char **argv) {
   char *uuid = malloc(37);
   uuid_unparse_lower(binuuid, uuid);
 
+#ifdef CGI
+  printf("Content-Type: application/json\n");
+  printf("\n");
+#endif
+
   printf("{\"data\":\"%s\",\"base64\":false,\"headers\":{},\"kv\":{},\"status\":200}\n", uuid);
 
   return 0;
